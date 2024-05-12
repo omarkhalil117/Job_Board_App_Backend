42eea4e37d284bd904867688fec4241b5ee182f4
SeifOSallam <seif.osallam@gmail.com>
2024-05-12 23:06:00 +0300

    merged and deleted migrations

--- app/Http/Resources/CandidateResource.php
+++ app/Http/Resources/CandidateResource.php
@@ -33,22 +33,3 @@ class CandidateResource extends JsonResource
         ];
     }
 }
-<?php
-
-namespace App\Http\Resources;
-
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
-
-class CandidateResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
-    }
-}
