cf71c3ee89561533486f0aa4cb41d7d12137ebbc
SeifOSallam <seif.osallam@gmail.com>
2024-05-18 14:07:54 +0300

    changed back to response

--- routes/api.php
+++ routes/api.php
@@ -110,8 +110,8 @@ Route::post('/forgot-password', function (Request $request) {
     );
  
     return $status === Password::RESET_LINK_SENT
-                ? back()->with(['status' => __($status)])
-                : back()->withErrors(['email' => __($status)]);
+                ? response()->json(['status' => __($status)])
+                : response()->json(['error' => __($status)]);
 })->middleware('guest')->name('password.email');
 
 Route::post('/reset-password', function (Request $request) {
